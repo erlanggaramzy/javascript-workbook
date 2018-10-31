843a27a775e2b3e780e93d72de702cca354ae379
Erlangga Ramzy <erlanggaramzy@gmail.com>
2018-10-30 20:58:58 -0500

    mastermind whiteboarding

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -1,5 +1,45 @@
 'use strict';
 
+//match user guess to a solution
+
+//user enters guess, lowercase, trim(steralize)
+  //guess has to be valid (isValid)(has to be in letters array, has to be guess of 4, cant be empty, can/cant repeat,)
+  //if not valid, tell why not valid, reprompt
+  //if guess is valid, 
+    //put guess on board. (pushToBoard) use push.
+    //check for win (checkForWin). guess matches solution (check if strings are the same)
+      //if match, print win message, reset, clear board
+      //before hint is given, check turns used (hasGuessRemaining)(board length or counter).
+        //if on turn 10, show solution, call them a loser, reset board (resetBoard)(its a win is similar write one reset function)
+        //if not on turn 10, give user a hint(showHint)
+          //hint: tell user how many letters are in right location and how many letters right letters (or wrong location wrong letter)
+
+  //mastermind() {
+  //  if isValid() {
+  //    pushToBoard() {
+  //      checkForWin() {
+  //        'congrats!';
+  //        reset();
+  //      } else {
+  //        hasGuessRemaining() {
+  //  
+  //        }
+  //      }
+  //    }
+  //  } else 'please use good input;
+  //  getPrompt()
+  //}
+
+// hint function, first want to check to see if letters are the same in the same place
+//1. split('') to split into an array,
+//2. loop through guess array (foreach)
+//for each letter in guess array, check for corresponding letter in solution array
+//if corresponding letter is equal, say its in the right place, and is the right letter
+//else, if letter is in solution array its right letter wrong place
+
+
+
+
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -32,9 +72,44 @@ function generateHint() {
   // your code here
 }
 
+const showHints=(guess)=>{
+  const guessArr = guess.split('');
+  const solutionArr = solution.split('');
+  let rightLetterRightPlace = 0;
+  let rightLetterWrongPlace = 0;
+  guessArr.forEach((letter, index)=>{
+    const correspondingLetter = solutionArr[index];
+    if (letter == solutionArr[index]) {
+      //add to right place right letter count
+      rightLetterRightPlace ++;
+    } else if (solutionArr.includes(letter)) {
+      //add to right letter wrong place count
+      rightLetterWrongPlace ++;
+    } //else not in solution array (dont need to write anything)
+  });
+  return `${rightLetterRightPlace}-${rightLetterWrongPlace}`;
+}
+
+
 function mastermind(guess) {
   solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+
+
+  // if (isValid(guess)) {
+  //   board.push(guess)
+  //   if(checkForWin(guess)) {
+  //     'tell user they won';
+  //     resetBoard();
+  //   } else if (!hasGuessesRemaining()) {
+  //     'tell user they lost and show solution'
+  //     reserBoard();
+  //   } else {
+  //     showHints()
+  //   }
+  // } else {
+  //   'please enter four letters from A to H'
+
+  // }
 }
 
 
